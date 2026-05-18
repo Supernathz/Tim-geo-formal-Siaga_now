02294577a00e76c4587eb92895e78d16b31fea60
Supernathz <Supernathz123@gmail.com>
2026-05-18 09:19:59 +0700

    Update and rename main.js to script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const title = document.getElementById("heroTitle");
+
+document.addEventListener("mousemove", (e) => {
+    const x = (e.clientX / window.innerWidth) * 100;
+    const y = (e.clientY / window.innerHeight) * 100;
+
+    title.style.setProperty("--x", `${x}%`);
+    title.style.setProperty("--y", `${y}%`);
+});
