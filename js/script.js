e3c0a34fcb9064194345fc480509b7d6a648c2c4
Supernathz <Supernathz123@gmail.com>
2026-05-18 15:04:40 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const title = document.querySelector(".hero-title");
 
-document.addEventListener("mousemove", (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
+title.addEventListener("mousemove", (e) => {
+  const rect = title.getBoundingClientRect();
+
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
 
   title.style.setProperty("--x", `${x}px`);
   title.style.setProperty("--y", `${y}px`);
